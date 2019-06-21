563b48f656b33c48d9171bd2d6a21661567dc3f0
BlackBeard-Teach <kmfoloe@student.wethinkcode.co.za>
2019-06-21 15:46:46 +0200

    Added minor implementation

--- com/blackbeard/teach/AircraftFactory.java
+++ com/blackbeard/teach/AircraftFactory.java
@@ -3,6 +3,10 @@ package com.blackbeard.teach;
 public class AircraftFactory {
     public Flyable newAircraft(String type, String name, int longitude, int latitude, int height)
     {
-
+        Flyable newOne = null;
+        if (type.equals("Baloon")){
+           newOne = new Baloon(name, new Coordinates(longitude, latitude, height));
+        }
+        return newOne;
     }
 }
