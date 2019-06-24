81924bac5119358972e0193f1af588b7d0e5be86
Keamo <39981840+BlackBeard-Teach@users.noreply.github.com>
2019-06-24 08:12:02 +0200

    Added useful functionality

--- com/blackbeard/teach/JetPlane.java
+++ com/blackbeard/teach/JetPlane.java
@@ -3,15 +3,48 @@ package com.blackbeard.teach;
 public class JetPlane extends Aircraft implements Flyable {
     WeatherTower weatherTower;
 
-    JetPlane(String name, Coordinates coordinates){}
+    /**
+     * JetPlane constructor with aurguments from the base class Aircraft
+     * @param name
+     * @param coordinates
+     */
+    JetPlane(String name, Coordinates coordinates){
+        //Flyable jet = new JetPlane(name, coordinates); incorrect
+        super(name, coordinates);
+    }
+
+    /**
+     * This will request the weather from the weatherTower and based on the
+     * conditions, it will modify the coordinates
+     * If the height of the flyable is 0 (meaning the flight has landed)
+     * it will unregister it from the weatherTower
+     */
 
     public void updateConditions()
     {
-
+        weatherTower.getWeather(coordinates);
+//        this.weatherTower.changeWeather();
+//        this.weatherTower.conditionsChanged();
+        //need to log the msg
+        if (coordinates.getHeight() == 0)
+        {
+            weatherTower.unregister(this);
+        }
     }
 
     public void registerTower(WeatherTower weatherTower)
     {
-
+//        Flyable jet = new JetPlane(name, coordinates);
+//        if (coordinates.getHeight() > 0)
+//        {
+//            weatherTower.register(jet);
+//        }
+//        else if (coordinates.getHeight() == 0)
+//        {
+//            weatherTower.unregister(jet);
+//        }
+        //Need to log the message
+        this.weatherTower = weatherTower;
+        weatherTower.register(this);
     }
 }
--- com/blackbeard/teach/Tower.java
+++ com/blackbeard/teach/Tower.java
@@ -1,20 +1,34 @@
 package com.blackbeard.teach;
 
+import java.util.ArrayList;
+
 public class Tower {
-    Flyable observers[];
+//    Flyable[] observers; better to use array list
+    private ArrayList<Flyable> observers = new ArrayList<>();
 
     public void register(Flyable flyable)
     {
-
+        //Don't know if this should be here but WTF
+        if (observers.contains(flyable))
+            return;
+        observers.add(flyable);
     }
+
+    /**
+     * UnRegisters the flyable if it exists
+     * @param flyable
+     */
     public void unregister(Flyable flyable)
     {
-
+        observers.remove(flyable);
     }
 
     protected void conditionsChanged()
     {
-
+        for (int i = 0; i < observers.size(); i++)
+        {
+            observers.get(i).updateConditions();
+        }
     }
 
 }
